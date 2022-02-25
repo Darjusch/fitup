6b321afc32f4a59c6c741fc7ef504692051e74be
Darjusch <Darjusch.schrand@code.berlin>
2022-02-25 16:43:27 +0100

    added test for bet execution

--- test/Fitup.js
+++ test/Fitup.js
@@ -9,12 +9,13 @@ contract("Fitup", accounts => {
     const account_one = accounts[1];
     const account_two = accounts[2];
     const organisation = accounts[3]; //Represents the organisation we donate to
+    const one_eth = ethers.utils.parseEther('1');
+
     let fitup;
     before( async () => {
         fitup = await Fitup.deployed();
     });
     it('creates a bet', async () => {
-        const one_eth = ethers.utils.parseEther('1');
         await fitup.createBet(
             organisation, { 
             from: account_one,
@@ -23,5 +24,22 @@ contract("Fitup", accounts => {
         bet = await fitup.getBet(account_one);
         expect(bet.organisation).to.equal(organisation);
         expect(bet.amount).to.equal(one_eth);
+    });
+    it('lost a bet', async() => {
+        const before = Number(ethers.utils.formatEther(await web3.eth.getBalance(organisation)));
+        await fitup.payoutBet(false, account_one);
+        const after = Number(ethers.utils.formatEther(await web3.eth.getBalance(organisation)));
+        expect(before).to.be.lessThan(after);
+    });
+    it('won a bet', async() => {
+        await fitup.createBet(
+            organisation, { 
+            from: account_one,
+            value: one_eth
+        });
+        const before = Number(ethers.utils.formatEther(await web3.eth.getBalance(account_one)));
+        await fitup.payoutBet(true, account_one);
+        const after = Number(ethers.utils.formatEther(await web3.eth.getBalance(account_one)));
+        expect(before).to.be.lessThan(after);
     });
 })
