518b76977a4db668de575d20b3fc43b73fedb695
Nameer-Alkhayyat <nameer.alkhayyat@code.berlin>
2022-03-05 16:28:15 +0100

    added createBet with validation

--- contracts/setBet.sol
+++ contracts/setBet.sol
@@ -4,21 +4,47 @@ pragma solidity 0.8.11;
 contract setBetContract {
 
 
-    uint256 public betCount = 0;
-     bool active=true;
+    uint256 public totalBetCount = 0;
+    uint256 public activeBetCount = 0;
+    bool active=true;
+
+
+    
     
 
    struct Bet {
         uint256 amount;
         address organisation;
-        address creator;
         uint256 created_at;
         bool active;
        
 
     }
-    mapping (address => Bet) bets;
+    mapping (address => Bet) public bets;
 
+
+
+
+
+    function createBet(uint256 amount,address organisation
+     ) external {
+        validateBet();
+        incrementCount();
+        address creator = msg.sender;
+        uint256 created_at = block.timestamp;
+
+        bets[creator] = Bet(amount, organisation, created_at, active);
+        
+    }
+
+    function validateBet() internal view {
+        require(msg.value > 0, "The bet value has to be bigger than 0");
+
+    }
+
+    function incrementCount() internal {
+        totalBetCount +=1;
+    }
     
 
   
