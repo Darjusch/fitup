fc5471e89b0318c30cb2bc8b4d18e41366613eb2
Nameer-Alkhayyat <nameer.alkhayyat@code.berlin>
2022-03-11 18:13:27 +0100

    updated createBet, added activebetcount

--- callFitup.js
+++ callFitup.js
@@ -2,7 +2,8 @@
 import Fitup from "./build/contracts/Fitup.json";
 import Web3 from "web3";
 
-const web3 = new Web3("http://127.0.0.1:8545");
+
+const web3 = new Web3("http://127.0.0.1:7545");
 const networkId = await web3.eth.net.getId();
 const deployedNetwork = Fitup.networks[networkId];
 
@@ -15,31 +16,49 @@ const someOrganisation = "0x1F78324b386d7EDa0CFd45947B8796B1F22053F4"
 
 function createBet() {
     console.log("Running createBet")
-    contract.methods.createBet(someOrganisation).send({from: "0xB3479377859d54feb715cEC2EA19b0Ea411c9b36", value: 1}, function (err, res) {
+    contract.methods.createBet(someOrganisation).send({ from: "0xB3479377859d54feb715cEC2EA19b0Ea411c9b36", value: 1 }, function (err, res) {
         if (err) {
             console.log("An error occured", err)
             return
         }
         console.log("Hash of the transaction: " + res)
         getBet()
-        getBalance();
-        getBetCount()
+        // getBalance();
+        // getBetCount()
     })
 }
 
 function getBet() {
-    contract.methods.getBet("0xB3479377859d54feb715cEC2EA19b0Ea411c9b36").call(function (err, res) {
+    contract.methods.totalBetCount().call(function (err, res) {
         if (err) {
             console.log("An error occured", err)
             return
         }
         console.log("Bet Data: " + res)
-       
+
     })
+
 }
 
+function getBalance() {
+
+    contract.methods.getBalance().call(function (err, res) {
+        if (err) {
+            console.log("An error occured", err)
+            return
+        }
+        console.log("Bet Data: " + res)
+
+    })
+
+}
+
+
+
+
+
 function payoutBet() {
-    contract.methods.payoutBet(true, accounts[0]).send({from: accounts[0]}, function (err, res) {
+    contract.methods.payoutBet(true, accounts[0]).send({ from: accounts[0] }, function (err, res) {
         if (err) {
             console.log("An error occured", err)
             return
@@ -50,19 +69,19 @@ function payoutBet() {
 
 
 }
-async function getBalance(){
-   const balance=  await web3.eth.getBalance("0xB3479377859d54feb715cEC2EA19b0Ea411c9b36")
-   
-    console.log(balance)
+
+
+async function getBetCount() {
+    const betCount = await contract.methods.getBetCount().call(function (err, res) {
+        if (err) {
+            console.log("An error occured", err)
+            return
+        }
+        console.log("Bet Data: " + res)
+
+    })
 
 
 }
 
-async function getBetCount(){
-    const betCount = await contract.methods.getBetCount();
-    console.log(betCount)
-
-}
-
-createBet()
 
--- contracts/Fitup.sol
+++ contracts/Fitup.sol
@@ -2,29 +2,29 @@
 pragma solidity 0.8.11;
 
 contract Fitup {
-    
     address owner;
     uint256 public totalBetCount = 0;
     uint256 public activeBetCount = 0;
-    bool active=false;
+    bool active = false;
 
-
-   struct Bet {
-    
+    struct Bet {
         uint256 amount;
         address organisation;
         uint256 created_at;
         bool active;
- 
-       
-
     }
-    mapping (address => Bet) public bets;
+
+    mapping(address => Bet) public bets;
 
     event BetCreated(address creator, address organisation, uint256 amount);
-    event BetPayout(address issuer, bool success, address organisation, uint256 amount);
+    event BetPayout(
+        address issuer,
+        bool success,
+        address organisation,
+        uint256 amount
+    );
 
-    modifier onlyOwner {
+    modifier onlyOwner() {
         require(msg.sender == owner);
         _;
     }
@@ -33,10 +33,9 @@ contract Fitup {
         owner = msg.sender;
     }
 
-    function createBet(address organisation
-     ) payable external {
-        validateBet();
+    function createBet(address organisation) external payable validateBet {
         incrementCount();
+        incrementActiveCount();
         uint256 amount = msg.value;
         address creator = msg.sender;
         uint256 created_at = block.timestamp;
@@ -44,45 +43,34 @@ contract Fitup {
 
         bets[creator] = Bet(amount, organisation, created_at, active);
         emit BetCreated(msg.sender, organisation, msg.value);
-    
     }
 
-
-    function getBet(address _from) external view returns(Bet memory) {
+    function getBet(address _from) external view returns (Bet memory) {
         return bets[_from];
     }
-    
+
     function payoutBet(bool _success, address _issuer) external onlyOwner {
         require(bets[_issuer].amount > 0);
         uint256 _amount = bets[_issuer].amount;
         address _organisation = bets[_issuer].organisation;
-        if(_success == true) {
+        if (_success == true) {
             payable(_issuer).transfer(_amount);
-        }
-        else {
+        } else {
             payable(_organisation).transfer(_amount);
         }
         emit BetPayout(_issuer, _success, _organisation, _amount);
     }
 
-
-    function validateBet() internal view {
-        require(msg.value > 0, "The bet value has to be bigger than 0");
-        // require(origanisation)
-
-    }
-
     function incrementCount() internal {
-        totalBetCount +=1;
+        totalBetCount += 1;
     }
 
-    function getBalance() public view returns (uint256) {
-        return address(this).balance;
+    function incrementActiveCount() internal {
+        activeBetCount += 1;
     }
 
-    function getBetCount() public view returns (uint ){
-        return totalBetCount;
-        
+    modifier validateBet() {
+        require(msg.value > 0, "The bet value has to be bigger than 0");
+        _;
     }
-    
 }
