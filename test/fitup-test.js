d0001739f1b1a7b3daff31d593faa0468afdd5ec
Darjusch <Darjusch.schrand@code.berlin>
2022-03-22 13:51:32 +0100

    fixed test

--- test/fitup-test.js
+++ test/fitup-test.js
@@ -1,65 +1,55 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
-const { isCallTrace } = require("hardhat/internal/hardhat-network/stack-traces/message-trace");
+const { expect } = require('chai')
+const { ethers } = require('hardhat')
 
-describe("Fitup", function () {
-
-
-  let Fitup;
+describe('Fitup', function () {
+  let Fitup
   let fitup
-  let owner;
-  let addr1;
-  let organisation;
-  let organisation2;
-  const value = {value: ethers.utils.parseEther("1.0")}
- beforeEach(async function (){
+  let owner
+  let addr1
+  let organisation
+  let organisation2
+  const value = { value: ethers.utils.parseEther('1.0') }
+  beforeEach(async function () {
+    Fitup = await ethers.getContractFactory('Fitup')
+    fitup = await Fitup.deploy()
+    await fitup.deployed()
+    ;[
+      owner,
+      addr1,
+      addr2,
+      organisation,
+      organisation2
+    ] = await ethers.getSigners()
 
-  Fitup = await ethers.getContractFactory("Fitup");
-  fitup = await Fitup.deploy();
-  await fitup.deployed();
-  [owner, addr1, addr2,  organisation, organisation2] = await ethers.getSigners();
-
-
-  await fitup.addNgo("CODE", organisation.address)
-  await fitup.createBet(
-    organisation.address, value);
-
-
- })
-  it("Should create a BET", async () => {
-  
-    console.log(owner.address)
-    expect(await fitup.createBet(organisation.address, value)).to.emit(fitup, "BetCreated").withArgs(owner.address, organisation.address, 1)
-
-});
-  // it("Should add a NGO", async () => {
-
-  //   await expect(fitup.addNgo(("UN", organisation2.address, "frf"))).to.emit(fitup, "NgoAdded").withArgs("UN", organisation2.address,"dmskmn")
-
-
-  // })
-  // it("Should payout a bet to a NGO", async () => {
-  //   expect(await fitup.payoutBet(false, owner.address)).to.emit(fitup, "BetPayout").withArgs(owner.address, false, organisation.address, 100000000)
-  // })
-  // it("Should payout a bet to the creator of the bet", async () => {
-  //   expect(await fitup.payoutBet(true, owner.address)).to.emit(fitup, "BetPayout").withArgs(owner.address, true, organisation.address, 100000000)
-  // })
-
-  it("Should check if NGO exist", async ()=>{
-    
+    await fitup.addNgo('CODE', organisation.address)
+    await fitup.createBet(organisation.address, value)
+  })
+  it('Should create a BET', async () => {
+    expect(await fitup.createBet(organisation.address, value))
+      .to.emit(fitup, 'BetCreated')
+      .withArgs(owner.address, organisation.address, 1)
+  })
+  it('Should add a NGO', async () => {
+    expect(await fitup.addNgo('UN', organisation2.address))
+      .to.emit(fitup, 'NgoAdded')
+      .withArgs('UN', organisation2.address)
+  })
+  it('Should payout a bet to a NGO', async () => {
+    expect(await fitup.payoutBet(false, owner.address))
+      .to.emit(fitup, 'BetPayout')
+      .withArgs(owner.address, false, organisation.address, value)
+  })
+  it('Should payout a bet to the creator of the bet', async () => {
+    expect(await fitup.payoutBet(true, owner.address))
+      .to.emit(fitup, 'BetPayout')
+      .withArgs(owner.address, true, organisation.address, value)
+  })
+  it('Should check if NGO exist', async () => {
     const ngoExist = await fitup.doesNgoExist(organisation.address)
     expect(ngoExist).to.be.equal(true)
-    
-    
   })
-
-  it("Should retun a bet", async ()=>{
-
-  const bet = await fitup.getBet(owner.address)
-  expect(bet.organisation).to.equal(organisation.address)
-
-
+  it('Should retun a bet', async () => {
+    const bet = await fitup.getBet(owner.address)
+    expect(bet.organisation).to.equal(organisation.address)
   })
-});
-
-
+})
