3920289a8f4c11c5d8aac259aa145be9205c915f
Darjusch <Darjusch.schrand@code.berlin>
2022-03-23 14:55:51 +0100

    gasreporter, task, test coverage

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,19 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
 let secrets = require("./secrets");
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
+require("hardhat-gas-reporter");
+require("@nomiclabs/hardhat-web3");
+require('solidity-coverage');
+
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
-    console.log(account.address);
+    var balance = await web3.eth.getBalance(account.address)
+    console.log(`${account.address} - ${web3.utils.fromWei(balance, "ether")} ETH`);
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
